17faaae66d48fc18f0d70c1fd1197d8f94c8f750
Vivek Gnanasekaran <vishavishal@gmail.com>
2021-09-08 18:41:24 +0530

    Dashboard for use added.

---
+++ app/controllers/dashboard_controller.rb
@@ -0,0 +1,9 @@
+class DashboardController < ApplicationController
+  def index
+    if user_signed_in?
+      redirect_to projects_path
+    else
+      redirect_to user_session_path
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root "project#index"
+  root "dashboard#index"
   get 'project/new'
+  get 'projects' => 'project#index'
+
   post 'project/create'
 end
